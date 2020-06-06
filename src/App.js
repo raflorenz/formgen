9d00bcda9b64cc6b04b1c4aacccb59184bb75d99
Raffy Lorenzana <rjlorenz931@gmail.com>
2020-06-07 02:16:54 +0800

    render form values on change below the form

--- src/App.js
+++ src/App.js
@@ -1,54 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [carCategory, setCarCategory] = useState('');
+  const [carModel, setCarModel] = useState('');
+
   return (
     <div className="app">
       <h1>Formgen</h1>
       <form>
         <input
           type="text"
+          value={name}
           className="form-control"
           placeholder="Name"
           required
+          onChange={e => setName(e.target.value)}
         />
         <input
           type="email"
+          value={email}
           className="form-control"
           placeholder="E-mail"
           required
+          onChange={e => setEmail(e.target.value)}
         />
         <input
           type="text"
+          value={phone}
           className="form-control"
           placeholder="Phone"
+          onChange={e => setPhone(e.target.value)}
         />
         <select
+          value={carCategory}
           className="form-control"
           required
+          onChange={e => setCarCategory(e.target.value)}
         >
-          <option>Car Category</option>
-          <option value="small">Small</option>
-          <option value="premium">Premium</option>
-          <option value="van">Van</option>
+          <option value="">Car Category</option>
+          <option value="Small">Small</option>
+          <option value="Premium">Premium</option>
+          <option value="Van">Van</option>
         </select>
         <select
+          value={carModel}
           className="form-control"
           required
+          onChange={e => setCarModel(e.target.value)}
         >
-          <option>Car Model</option>
-          <option value="opel-corsa">Opel Corsa</option>
-          <option value="toyota-yaris">Toyota Yaris</option>
-          <option value="smart-for-two">Smart for Two</option>
-          <option value="audi-s8">Audi S8</option>
-          <option value="jaguar-xjr">Jaguar XJR</option>
-          <option value="bmw-750il">BMW 750iL</option>
-          <option value="volkswagen-touran">Volkswagen Touran</option>
-          <option value="renault-espace">Renault Espace</option>
-          <option value="fiat-talento">Fiat Talento</option>
+          <option value="">Car Model</option>
+          <option value="Opel Corsa">Opel Corsa</option>
+          <option value="Toyota Yaris">Toyota Yaris</option>
+          <option value="Smart for Two">Smart for Two</option>
+          <option value="Audi S8">Audi S8</option>
+          <option value="Jaguar XJR">Jaguar XJR</option>
+          <option value="BMW 750iL">BMW 750iL</option>
+          <option value="Volkswagen Touran">Volkswagen Touran</option>
+          <option value="Renault Espace">Renault Espace</option>
+          <option value="Fiat Talento">Fiat Talento</option>
         </select>
         <button className="form-control">Submit</button>
       </form>
+      <div>
+        <p>Name: {name}</p>
+        <p>E-mail: {email}</p>
+        <p>Phone: {phone}</p>
+        <p>Car Category: {carCategory}</p>
+        <p>Car Model: {carModel}</p>
+      </div>
     </div>
   );
 }
